cd05f551a52933a55545d9f8f061668e9375599f
Anton <w.zzzz@mail.ru>
2020-01-10 06:32:45 +0300

    tests

--- src/test/java/com/Sales/SalesWeb/SaleWebApplicationTests.java
+++ src/test/java/com/Sales/SalesWeb/SaleWebApplicationTests.java
@@ -1,11 +1,19 @@
 package com.Sales.SalesWeb;
 
+import com.services.serviceDownloadsVersions.controller.DownloadController;
+import com.services.serviceDownloadsVersions.model.Download;
+import com.services.serviceDownloadsVersions.service.DownloadsService;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
 
 class SaleWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void genericTest() {
+        DownloadController downloadController = new DownloadController(new DownloadsService());
+        ResponseEntity actualVersions = downloadController.getActualVersions();
+        if (actualVersions.getBody().getClass() != Download.class) {
+            throw new RuntimeException("Test error");
+        }
+    }
 }
